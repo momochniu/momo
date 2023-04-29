971d0ea01107c55da9df289b213a9642d1f4524b
momochniu <qingzhengliulianshu@gmail.com>
2023-04-29 09:30:24 +0800

    [basic utils] Add map_diff_op.

--- include/basic/mbasic.h
+++ include/basic/mbasic.h
@@ -3,34 +3,65 @@
 
 #include <vector>
 #include <numeric>
+#include <map>
+#include <functional>
 
 namespace momo{
 
-template<typename T,std::integer_sequence<size_t,size_t... exp>>//power(T{},std::make_sequence<8>{})
-constexpr T power(T base){
-    T ret = 1;
-    ((ret *= base,exp),...);
-    for(int i = 0; i < exp; ++i){
-        ret *= base;
+template <typename K, typename V>
+void map_diff_op(const std::map<K, V> &new_map, const std::map<K, V> &old_map,
+            std::function<void (const K &key, const V &value)> add,
+            std::function<void (const K &key, const V &new_value, const V &old_value)> change = nullptr,
+            std::function<void (const K &key, const V &value)> del = nullptr,
+            std::function<void (const K &key, const V &value)> eq = nullptr,
+            std::function<bool (const V &lhs, const V &rhs)> op = [](const V &lhs, const V &rhs) -> bool {
+                                                                        return lhs == rhs;
+                                                                    })
+{
+    auto it_new = new_map.begin();
+    auto it_old = old_map.begin();
+    auto it_new_end = new_map.end();
+    auto it_old_end = old_map.end();
+    while (it_old != it_old_end && it_new != it_new_end) {
+        if (it_old->first == it_new->first) {
+            if (eq || change) {
+                if (op(it_new->second, it_old->second)) {
+                    if (eq) {
+                        eq(it_new->first, it_new->second);
+                    }
+                } else {
+                    if (change) {
+                        change(it_new->first, it_new->second, it_old->second);
+                    }
+                }
+            }
+            ++it_old;
+            ++it_new;
+        } else if (it_old->first < it_new->first) {
+            if (del) {
+                del(it_old->first, it_old->second);
+            }
+            ++it_old;
+        } else {
+            if (add) {
+                add(it_new->first, it_new->second);
+            }
+            ++it_new;
+        }
     }
-    return ret;
-}
 
+    while (it_old != old_map.end()) {
+        if (del) {
+            del(it_old->first, it_old->second);
+        }
+        ++it_old;
+    }
 
-
-template<typename T, T... ints>
-T sequence_product(std::integer_sequence<T, ints...> int_seq) {
-    T ret = 1;
-    ((ret *= ints),...);
-    return ret;
-}
-
-template<typename T>
-constexpr T power(T base, int exp) {
-    if constexpr(exp == 1){
-        return base;
-    }else{
-        return base * power(base, exp - 1);
+    while (it_new != new_map.end()) {
+        if (add) {
+            add(it_new->first, it_new->second);
+        }
+        ++it_new;
     }
 }
 
@@ -56,7 +87,7 @@ template<class>
 inline constexpr bool dependent_false_v = false;
 
 template<typename T>
-auto from_struct_to_reference_tuple(T&){
+auto from_struct_to_reference_tuple(T&) {
     static_assert(dependent_false_v<T>, "need a definition for `auto from_struct_to_tuple(T&)`");
     return T{};
 }
